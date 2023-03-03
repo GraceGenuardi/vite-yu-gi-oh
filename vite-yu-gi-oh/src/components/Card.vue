600c343398d2985576e1a82faf2b2be8af1534fb
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-03 16:06:34 +0100

    FIX risolto problema API

---
+++ vite-yu-gi-oh/src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card-grid">
+    <div v-for="card in cards" :key="card.id" class="card">
+      <img :src="card.imageUrl" :alt="card.name" class="card-image" />
+      <h3 class="card-title">{{ card.name }}</h3>
+      <p class="card-text">{{ card.description }}</p>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    cards: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+  
+<style scoped>
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 20px;
+}
+
+.card {
+  background-color: #ffffff;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.card-image {
+  width: 100%;
+  max-height: 200px;
+  object-fit: cover;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.card-title {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+}
+
+.card-text {
+  font-size: 1rem;
+  line-height: 1.5;
+  margin-bottom: 10px;
+}
+</style>
--- vite-yu-gi-oh/src/components/Main.vue
+++ vite-yu-gi-oh/src/components/Main.vue
@@ -1,114 +1,98 @@
 <template>
-    <main class="main">
+  <main class="main">
 
-      <!-- Menù dropdown -->
-      <div class="menu">
-         <ul>
-           <li class="dropdown">
-           <a href="#" class="alien">Alien</a> <i class="fa-solid fa-chevron-down"></i>
-           <div class="dropdown-content">
-             <a href="#">Fedele della luce</a>
-             <a href="#">Ala Nera</a>
-             <a href="#">Gladiatore Bestia</a>
-           </div>
-           </li>
-          </ul>
-        </div>
+    <!-- Menù dropdown -->
+    <div class="menu">
+      <ul>
+        <li class="dropdown">
+          <a href="#" class="alien">Alien</a> <i class="fa-solid fa-chevron-down"></i>
+          <div class="dropdown-content">
+            <a href="#">Fedele della luce</a>
+            <a href="#">Ala Nera</a>
+            <a href="#">Gladiatore Bestia</a>
+          </div>
+        </li>
+      </ul>
+    </div>
 
-        <!-- FINE MENù -->
-      
-      <div class="container-main">
+    <!-- FINE MENù -->
+
+    <div class="container-main">
 
 
-        <ul class="characters">
-  
-          
-          <Character v-for="element in store.characters" :key="element.id" :character="element" />
-  
-        </ul>
-      </div>
-    </main>
-  </template>
-  
-  <script>
-    import axios from 'axios'
-    import store from '../store'
-    import Character from './Character.vue'
-    export default {
-      components: {
-        Character
-      },
-      data() {
-        return {
-          // characters: [],
-          info: {},
-          store,
-          // store: store
-        }
-      },
-      computed: {
-        characters() {
-          return this.store.characters
-        }
-      },
-      methods: {
-        fetchCharacters() {
-          console.log('fetching data')
-          // fare la chiamata in get all'endpoint: 
-          axios
-            .get('https://db.ygoprodeck.com/api/character')
-            .then((res) => {
-              console.log(res)
-              console.log(res.data)
-              console.log(res.data.info)
-              this.store.characters = res.data.results
-              // this.info = res.data.info
-              const { count, pages } = res.data.info
-              this.store.count = count
-              this.store.pages = pages
-              console.log(this.store)
-            })
-        }
-      },
-      created() {
-        console.log('store',this.store)
-        this.fetchCharacters()
-      },
+      <ul class="cards">
+
+
+        <Card v-for="card in cards" :key="card.id" :card="card" />
+
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+import Card from './Card.vue'
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      cards: []
     }
-  </script>
-  
-  <style lang="scss" scoped>
-  .main {
-    padding: 50px 0;
-    background-color: #D48F38;
-   
-    
-  }
-  .container-main{
-    width: 1000px;
-    margin-left: 500px;
-  }
-  .characters {
-    display: grid;
-    gap: 40px;
-    grid-template-columns: repeat(4,1fr);
-    background-color: white;
-    
-  }
+  },
+  methods: {
+    fetchCards() {
+      console.log('fetching data')
+      axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then((res) => {
+          console.log(res)
+          console.log(res.data)
+          this.cards = res.data.data
+        })
+    }
+  },
+  created() {
+    this.fetchCards()
+  },
+}
+</script>
 
-  /**MENU RULES */
+<style lang="scss" scoped>
+.main {
+  padding: 50px 0;
+  background-color: #D48F38;
 
-  .menu {
-    background-color: white;
-    margin-bottom: 10px;
-    display: flex;
-    width: 120px;
-    margin-left: 550px;
-    padding: 5px;
-    border-radius: 10px;
-  }
 
-  .dropdown-content {
+}
+.container-main{
+  width: 1000px;
+  margin-left: 500px;
+}
+.cards {
+  display: grid;
+  gap: 40px;
+  grid-template-columns: repeat(4,1fr);
+  background-color: white;
+
+}
+
+/**MENU RULES */
+
+.menu {
+  background-color: white;
+  margin-bottom: 10px;
+  display: flex;
+  width: 120px;
+  margin-left: 550px;
+  padding: 5px;
+  border-radius: 10px;
+}
+
+.dropdown-content {
   display: none;
   position: absolute;
   z-index: 1;
@@ -134,5 +118,4 @@
 .alien {
   padding-right: 40px;
 }
-  
-  </style>
+</style>
